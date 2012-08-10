952aabe76fd883c5149983ed3ce838906870d955
Gustavo Genovese <gcesarmza@gmail.com>
2012-08-09 22:36:39 -0300

    Fixed operands order

--- calculator/src/main/java/com/gustavogenovese/calculator/CalculatorModel.java
+++ calculator/src/main/java/com/gustavogenovese/calculator/CalculatorModel.java
@@ -4,8 +4,13 @@ public class CalculatorModel {
 	private int tempBuffer;
 	private int operationBuffer;
 	private int operation;
+	private boolean resetInput = true;
 	
 	public void digitPressed(int digit){
+		if (resetInput){
+			tempBuffer = 0;
+			resetInput = false;
+		}
 		if (digit<0 || digit >9)
 			return;
 		tempBuffer = 10*tempBuffer + digit;
@@ -20,26 +25,26 @@ public class CalculatorModel {
 			return;
 		this.operation = operation;
 		operationBuffer = tempBuffer;
-		tempBuffer = 0;
+		resetInput = true;
 	}
 	
 	public void oper(){
 		
 		switch (operation){
 		case 0:
-			tempBuffer += operationBuffer; 
+			tempBuffer = operationBuffer + tempBuffer; 
 			break;
 			
 		case 1:
-			tempBuffer -= operationBuffer;
+			tempBuffer = operationBuffer - tempBuffer;
 			break;
 			
 		case 2:
-			tempBuffer *= operationBuffer;
+			tempBuffer = operationBuffer * tempBuffer;
 			break;
 			
 		case 3:
-			tempBuffer /= operationBuffer;
+			tempBuffer = operationBuffer / tempBuffer;
 			break;
 		}
 	}
