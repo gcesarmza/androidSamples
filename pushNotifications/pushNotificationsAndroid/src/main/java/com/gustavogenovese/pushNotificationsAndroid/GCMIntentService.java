55f091d9e849fd7fd52861cfbcd8dbaadfebfeeb
Gustavo Genovese <gcesarmza@gmail.com>
2012-08-25 15:39:03 -0300

    Added android app for push notifications

---
+++ pushNotifications/pushNotificationsAndroid/src/main/java/com/gustavogenovese/pushNotificationsAndroid/GCMIntentService.java
@@ -0,0 +1,33 @@
+package com.gustavogenovese.pushNotificationsAndroid;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gcm.GCMBaseIntentService;
+
+public class GCMIntentService extends GCMBaseIntentService{
+
+	private final static String TAG = "pushNotificationsAndroid-GCMIntentService";
+	
+	@Override
+	protected void onError(Context context, String errorId) {
+		Log.i(TAG, "onError: " + errorId);
+	}
+
+	@Override
+	protected void onMessage(Context context, Intent intent) {
+		Log.i(TAG, "onMessage");
+	}
+
+	@Override
+	protected void onRegistered(Context context, String regId) {
+		Log.i(TAG, "onRegistered: " + regId);		
+	}
+
+	@Override
+	protected void onUnregistered(Context context, String regId) {
+		Log.i(TAG, "onUnregistered: " + regId);
+	}
+
+}
---
+++ pushNotifications/pushNotificationsAndroid/src/main/java/com/gustavogenovese/pushNotificationsAndroid/HelloAndroidActivity.java
@@ -0,0 +1,38 @@
+package com.gustavogenovese.pushNotificationsAndroid;
+
+import com.google.android.gcm.GCMRegistrar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloAndroidActivity extends Activity {
+
+	private static String TAG = "pushNotificationsAndroid";
+	private final static String SENDER_ID = "683138750212";
+
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+	 *            is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.i(TAG, "onCreate");
+		setContentView(R.layout.main);
+
+		GCMRegistrar.checkDevice(this);
+		GCMRegistrar.checkManifest(this);
+		final String regId = GCMRegistrar.getRegistrationId(this);
+		if (regId.equals("")) {
+			GCMRegistrar.register(this, SENDER_ID);
+		} else {
+			Log.v(TAG, "Already registered");
+		}
+	}
+
+}
